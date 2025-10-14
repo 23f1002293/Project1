e145d2128788cfce9d93a265b882669dc917db0a
23f1002293 <23f1002293@ds.study.iitm.ac.in>
2025-10-14 17:43:43 +0000

    Added verification code

--- app/main.py
+++ app/main.py
@@ -1,9 +1,31 @@
 from fastapi import FastAPI
+import os
+
+def round1():
+    pass
+
+def round2():
+    pass
 
 app = FastAPI()
 
+def validate_secret(secret: str) -> bool:
+    return secret == os.getenv("SECRET")
+
+
 @app.post("/handle_task")
 async def handle_task(data: dict):
+    if not validate_secret(data.get("secret", "")):
+        return {"error": "Invalid secret"}
+    else:
+        if data.get("round") == 1:
+            round1()
+        elif data.get("round") == 2:
+            round2()
+        else:
+            return {"error": "Invalid round"}
+        pass
+    return {"message": "Task recieved", "data": data}
     print(data)
     return {"status": "Task received", "data": data}
 
@@ -14,3 +36,4 @@ async def handle_task(data: dict):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+    
